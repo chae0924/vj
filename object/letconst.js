65febe9d58e8d88620a2aa3a9df3ca7501fa7f35
chae0924 <ws0924@gmail.com>
2024-09-24 12:53:02 +0900

    셀렉트형 네비게이션 DB 분리하고 접근하기/ECMA 6기법

--- object/letconst.js
+++ object/letconst.js
@@ -1,20 +1,40 @@
 const family = {
-        d1text : "패밀리사이트",
+        d1text : "RealativeSite",
         familysite : [
-            "네이버","구글","다음","유튜브","쿠팡"
+          {
+            atext : "GIT",
+            href : "http://www.github.com/chae0924",
+          },
+          {
+            atext : "Notion",
+            href : "http://www.notion.so",
+          },
+          {
+            atext : "Figma",
+            href : "http://www.figma.com",
+          },
         ]
 }
 
 // 터미널에 node object/letconst  파일 안 object안 letconst의 콘솔 명령어 실행
 
-console.log(family.familysite.join(", "))
+// console.log(family.familysite[0].atext, family.familysite[0].href);
+// const mytag =`<a href="${family.familysite[2].href}"> ${family.familysite[2].atext}</a>`;
 
-for( let x in family.familysite ){
-  // index
-  console.log(x)
+// console.log(mytag);
+
+
+
+
+// 모든 항목 출력 (for...of)
+for (const site of family.familysite) {
+  console.log(site.atext, site.href);
 }
 
-for( let x of family.familysite ){
-  // value
-  console.log(x)
+// HTML 태그 생성
+let mytags = '';
+for (const site of family.familysite) {
+  mytags += `<a href="${site.href}">${site.atext}</a> `;
 }
+
+console.log(mytags);
