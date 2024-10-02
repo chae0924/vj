de666a66179a58de0d466d8e1f7cf14d3d7226ee
chae0924 <ws0924@gmail.com>
2024-10-02 10:44:44 +0900

    241001_scss작성

---
+++ object/basic.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',function(){
+  const allMenubtn = document.querySelector("#hd_allmenu_bby");
+  allMenubtn.addEventListener('click', function(){
+    document.body.classList.toggle('menuOpen')
+  })
+  })
