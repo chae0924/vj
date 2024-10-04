93e9eb6992bd5901610300f7027198ad67001c14
chae0924 <ws0924@gmail.com>
2024-10-04 10:05:53 +0900

    소메뉴스크립트 훈련_01

--- object/basic.js
+++ object/basic.js
@@ -3,4 +3,27 @@ window.addEventListener('load',function(){
   allMenubtn.addEventListener('click', function(){
     document.body.classList.toggle('menuOpen')
   })
-  })
+  
+
+
+
+  //여기서
+  // const Menufirst = this.document.mynavi.title[1];
+  // const Menufirst = this.document.querySelector("#navi>ul>li>a");
+  // Menufirst.innerHTML="ccc"
+  // this.document.querySelector("#navi>ul>li>a").innerHTML += "cc";
+  // this.document.querySelectorAll("#navi>ul>li>a")[1].innerHTML += "ccccc";
+
+  for(x in mynavi){
+  this.document.querySelectorAll("#navi>ul")[x].innerHTML = `<li class="position-relative">
+            <a href="" class="d-block text-nowarp">
+            ${mynavi[x].title}
+            </a>
+            <div class="position-absolute">
+              <ul>`;
+              // for
+
+  this.document.querySelectorAll("#navi>ul")[x].innerHTML +=  ` </ul></div>`;
+}
+
+})
--- object/letconst.js
+++ object/letconst.js
@@ -40,10 +40,71 @@ for( x of family.familysite ){
       familytag += `<li>${x.atext}</li>`
 }
 familytag +=`</ul>`
-
+// 패밀리노출
 window.addEventListener('load',function(){
-      document.querySelector("#family ul").innerHTML = familytag;
+      // document.querySelector("#family ul").innerHTML = familytag;
 
 });
 
+const mynavi = [
+  {
+      title: "대박적",
+      href: "#long-menu",
+      submenu: [
+          { text: "소뉴 1", href: "#submenu1" },
+          { text: "소뉴 2", href: "#submenu2" },
+          { text: "소메뉴 3", href: "#submenu3" },
+          { text: "소메뉴 4", href: "#submenu4" },
+          { text: "소메뉴 5", href: "#submenu5" }
+      ]
+  },
+  {
+      title: "div얖 class침ㄴㄴ",
+      href: "#menu1",
+      submenu: [
+          { text: "소뉴 1", href: "#submenu1-1" },
+          { text: "소뉴 2", href: "#submenu1-2" },
+          { text: "소메뉴 3", href: "#submenu1-3" },
+          { text: "소메뉴 4", href: "#submenu1-4" },
+          { text: "찾아오시는 길", href: "#long-path" }
+      ]
+  },
+  {
+      title: "대메뉴 2",
+      href: "#menu2",
+      submenu: [
+          { text: "소메뉴 1", href: "#submenu2-1" },
+          { text: "소메뉴 2", href: "#submenu2-2" },
+          { text: "소메뉴 3", href: "#submenu2-3" },
+          { text: "소메뉴 4", href: "#submenu2-4" },
+          { text: "소메뉴 5", href: "#submenu2-5" }
+      ]
+  },
+  {
+      title: "대메뉴 3",
+      href: "#menu3",
+      submenu: [
+          { text: "소메뉴 1", href: "#submenu3-1" },
+          { text: "소메뉴 2", href: "#submenu3-2" },
+          { text: "소메뉴 3", href: "#submenu3-3" },
+          { text: "소메뉴 4", href: "#submenu3-4" },
+          { text: "소메뉴 5", href: "#submenu3-5" }
+      ]
+  },
+  {
+      title: "대메뉴 4",
+      href: "#menu4",
+      submenu: [
+          { text: "소메뉴 1", href: "#submenu4-1" },
+          { text: "소메뉴 2", href: "#submenu4-2" },
+          { text: "소메뉴 3", href: "#submenu4-3" },
+          { text: "소메뉴 4", href: "#submenu4-4" },
+          { text: "소메뉴 5", href: "#submenu4-5" }
+      ]
+  }
+];
+
+for(z in mynavi.submenu){
+  // this.document.querySelectorAll("#navi>ul>li>a")[z].innerHTML = z.text;
+}
 
