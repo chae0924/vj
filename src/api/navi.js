3eb2120048849e9b57d70e2c9ab0ff5f24355a5a
chae0924 <ws0924@gmail.com>
2024-10-10 10:33:02 +0900

    navi함수를 활용한 사전인터뷰 페이지 구축 #1

--- src/api/navi.js
+++ src/api/navi.js
@@ -38,6 +38,28 @@ window.addEventListener('load',function(){
   this.document.querySelector("#family ul").innerHTML = familylist;
 
 
+
+
+  // faq
+
+  let faqtag = ""; // 값 변경 가능한 변수를 선언해준다
+  const faqtarget =  this.document.querySelector("#faqcontent  .content"); // 바뀌지 않는 변수 선언
+
+  faqtag += `<div class="container py-5">`;
+  // x 변수가 같아도 for구문 밖으로 못나가므로 상관없음
+  for( x of faqcontent ){
+            faqtag += `<dl class="py-1"><dt class="border-bottom py-2 fs-4"> ${x.faqQ}</dt>
+            <dd class="py-2 fs-5">${
+              // console.log(x.faqA.split("|"),  Array.isArray(x.faqA.split("|"))  )
+              x.faqA.split("|").join("<br>")
+
+            }</dd></dl>`;
+            }
+    faqtag += `</div>`;
+  faqtarget.innerHTML = faqtag;
+
+
+
   //패밀리노출
   this.document.querySelector("#family h2").addEventListener('click', function(){
     this.classList.toggle('open');
